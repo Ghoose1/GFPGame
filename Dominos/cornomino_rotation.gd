7b993fb4ddbd7c869637996369724f0b811d275b
Ghoose <78841748+Ghoose1@users.noreply.github.com>
2026-03-26 16:13:25 +1100

    small cleanup

--- Dominos/cornomino_rotation.gd
+++
@@ -1,8 +0,0 @@
-extends Sprite2D
-
-@onready var domino : Domino = get_parent().get_parent()
-
-func _process(_delta: float) -> void:
-	frame = domino.rotation_direction
-	global_rotation = 0
-	queue_redraw()
