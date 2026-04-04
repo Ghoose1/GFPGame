a3673658ca556e59cdf87e39ebf4710741b08613
Ghoose <78841748+Ghoose1@users.noreply.github.com>
2026-04-04 13:35:04 +1100

    changed main menu scene tree and integrated level selection menu

--- scenes/main_menu.gd
+++ scenes/main_menu.gd
@@ -1,8 +1,6 @@
 extends Node2D
 
 @onready var start_button: Button = %"Start Button"
-@onready var start: ColorRect = $Start
-@onready var start_label: Label = $"Start/Start Label"
 
 func _ready() -> void:
 	start_button.grab_focus() 
