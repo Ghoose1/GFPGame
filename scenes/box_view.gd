9ccdd43e433947cc0c8ccd67427c5cce14e15d9b
abdi-one <abdilahi.b.abdi@gmail.com>
2026-04-08 23:14:38 +1000

    Main Menu Changes

--- scenes/box_view.gd
+++ scenes/box_view.gd
@@ -9,8 +9,5 @@ extends Node2D
 
 
 #Returns user back to the scene
-
-
-
 func _on_exit_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/scene.tscn")
--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -7,3 +7,9 @@ func _ready() -> void:
 
 func _on_start_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/level_select_menu.tscn")
+
+func _on_option_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/Options.tscn")
+	
+func _on_exit_button_pressed() -> void:
+	get_tree().quit()
