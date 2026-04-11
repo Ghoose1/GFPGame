dc3ed72193d33cba2bec29813bceec0cbaae7a10
Ghoose <78841748+Ghoose1@users.noreply.github.com>
2026-04-11 11:14:37 +1000

    box button is invisible now

---
+++ scenes/main_menu.gd
@@ -0,0 +1,9 @@
+extends Node2D
+
+@onready var start_button: Button = %"Start Button"
+
+#func _ready() -> void:
+	#start_button.grab_focus() 
+
+func _on_start_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/level_select_menu.tscn")
