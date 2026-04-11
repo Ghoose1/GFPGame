4d163ed8d5c2aae3735349a0c1f4e77eabc62610
Ghoose <78841748+Ghoose1@users.noreply.github.com>
2026-04-11 11:49:35 +1000

    added some comments:

--- scenes/box_view.gd
+++ scenes/box_view.gd
@@ -1,15 +1,12 @@
 extends Control
 
-#Returns user back to the scene
-#func _on_exit_pressed() -> void:
-	#get_tree().change_scene_to_file("res://scenes/scene.tscn")
-
 @onready var dominoes_node : Node = $Dominoes
 
 func _ready() -> void:
 	hide()
 
 func open() -> void:
+	# slide onto the screen from below
 	show()
 	var tween := create_tween()
 	tween.tween_property(self, "global_position", global_position, 0.2)
@@ -19,16 +16,20 @@ func open() -> void:
 	
 	var index := 0
 	for domino : Domino in dominoes:
+		# clone the domino
 		var clone := domino.duplicate()
 		clone.is_clone = true
+		# set the transform
 		clone.position = Vector2(floor(index / 7.0), index % 7) * 36
 		clone.rotation = 0
 		clone.show()
+		# The clones are added to this node to make cleaning up easier
 		dominoes_node.add_child(clone)
 		index += 1
-		#clone.position = Vector2(floor(index / 7.0), index % 7) * 24
 
 func close() -> void:
+	# remove all the cloned children
 	for child in dominoes_node.get_children():
 		child.queue_free()
+	# hide this menu
 	hide()
